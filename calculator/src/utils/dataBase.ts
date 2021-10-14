9f4355af862ec4a0fe2bf5abd362c00611706ebc
Artsiom Murashko <ksarelto@tut.by>
2021-10-14 17:16:30 +0300

    feat: create dataBase

---
+++ calculator/src/utils/dataBase.ts
@@ -0,0 +1,4 @@
+export const NumbersArray =  [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+export const OperatorsArray = ['+', '-', '*', '/', '='];
+export const AdditionalUtilities = ['AC', 'MEM +', 'MEM -'];
+  
